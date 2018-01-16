8e750cc9db99a3ef143336876b94cd62354dc791
shawn <shawntoubeau@gmail.com>
2018-01-15 22:30:46 -0500

    Made changes

--- quick-meetings.js
+++ quick-meetings.js
@@ -1,23 +1,23 @@
   function start() {
-        // 2. Initialize the JavaScript client library.
-        gapi.client.init({
-          'apiKey': 'AIzaSyDX1NIXf8-dc7ZC4XXcipbrZgow59MsAfQ',
-          // Your API key will be automatically added to the Discovery Document URLs.
-          'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
-          // clientId and scope are optional if auth is not required.
-          'clientId': '208833000673-qkfrncgkctrhfp0tbvbb0lefbv1ibt2n.apps.googleusercontent.com',
-          'scope': 'profile',
-        }).then(function() {
-          // 3. Initialize and make the API request.
-          return gapi.client.calendar.events.list({
-            'calendarId': 'wentworthcsa@wit.edu',
-            'orderBy': "startTime",
-            'singleEvents': true
-          });
-        }).then(function(response) {
-          var eventTitle = JSON.stringify(response.result.items[0].summary).replace(/["]+/g, '');
-          var description = JSON.stringify(response.result.items[0].description).replace(/["]+/g, '');
-          var time = JSON.stringify(response.result.items[0].start.dateTime);
+    // 2. Initialize the JavaScript client library.
+    gapi.client.init({
+      'apiKey': 'AIzaSyDX1NIXf8-dc7ZC4XXcipbrZgow59MsAfQ',
+      // Your API key will be automatically added to the Discovery Document URLs.
+      'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
+      // clientId and scope are optional if auth is not required.
+      'clientId': '208833000673-qkfrncgkctrhfp0tbvbb0lefbv1ibt2n.apps.googleusercontent.com',
+      'scope': 'profile',
+    }).then(function() {
+      // 3. Initialize and make the API request.
+      return gapi.client.calendar.events.list({
+        'calendarId': 'wentworthcsa@wit.edu',
+        'orderBy': "startTime",
+        'singleEvents': true
+      });
+    }).then(function(response) {
+      var eventTitle = JSON.stringify(response.result.items[0].summary).replace(/["]+/g, '');
+      var description = JSON.stringify(response.result.items[0].description).replace(/["]+/g, '');
+      var time = JSON.stringify(response.result.items[0].start.dateTime);
 
       document.getElementById('event').innerHTML = eventTitle;
       document.getElementById('description').innerHTML = description;
