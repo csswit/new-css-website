a554c5839db4bbd234652a65ad2137c314648155
shawn <shawntoubeau@gmail.com>
2018-01-15 22:14:43 -0500

    Restricted api key

--- quick-meetings.js
+++ quick-meetings.js
@@ -1,42 +1,37 @@
-const dotenv = require('dotenv');
-dotenv.load();
+  function start() {
+        // 2. Initialize the JavaScript client library.
+        gapi.client.init({
+          'apiKey': 'AIzaSyDX1NIXf8-dc7ZC4XXcipbrZgow59MsAfQ',
+          // Your API key will be automatically added to the Discovery Document URLs.
+          'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
+          // clientId and scope are optional if auth is not required.
+          'clientId': '208833000673-qkfrncgkctrhfp0tbvbb0lefbv1ibt2n.apps.googleusercontent.com',
+          'scope': 'profile',
+        }).then(function() {
+          // 3. Initialize and make the API request.
+          return gapi.client.calendar.events.list({
+            'calendarId': 'wentworthcsa@wit.edu',
+            'orderBy': "startTime",
+            'singleEvents': true
+          });
+        }).then(function(response) {
+          var eventTitle = JSON.stringify(response.result.items[0].summary).replace(/["]+/g, '');
+          var description = JSON.stringify(response.result.items[0].description).replace(/["]+/g, '');
+          var time = JSON.stringify(response.result.items[0].start.dateTime);
 
-var apiKey = process.env.APIKEY;
+      document.getElementById('event').innerHTML = eventTitle;
+      document.getElementById('description').innerHTML = description;
+      document.getElementById('time').innerHTML = extractTime(time);
+    }).catch((err) => {
+      console.log(err);
+    })
+  };
 
-function start() {
-      // 2. Initialize the JavaScript client library.
-      gapi.client.init({
-        'apiKey': apiKey,
-        // Your API key will be automatically added to the Discovery Document URLs.
-        'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
-        // clientId and scope are optional if auth is not required.
-        'clientId': '208833000673-qkfrncgkctrhfp0tbvbb0lefbv1ibt2n.apps.googleusercontent.com',
-        'scope': 'profile',
-      }).then(function() {
-        // 3. Initialize and make the API request.
-        return gapi.client.calendar.events.list({
-          'calendarId': 'wentworthcsa@wit.edu',
-          'orderBy': "startTime",
-          'singleEvents': true
-        });
-      }).then(function(response) {
-        var eventTitle = JSON.stringify(response.result.items[0].summary).replace(/["]+/g, '');
-        var description = JSON.stringify(response.result.items[0].description).replace(/["]+/g, '');
-        var time = JSON.stringify(response.result.items[0].start.dateTime);
-
-    document.getElementById('event').innerHTML = eventTitle;
-    document.getElementById('description').innerHTML = description;
-    document.getElementById('time').innerHTML = extractTime(time);
-  }).catch((err) => {
-    console.log(err);
-  })
-};
-
-function extractTime(time) {
-  var year = time.slice(1, 5);
-  var month = time.slice(6, 8);
-  var day = time.slice(9, 11);
-  return month + '/' + day + '/' + year;
-}
-// 1. Load the JavaScript client library.
-gapi.load('client', start);
+  function extractTime(time) {
+    var year = time.slice(1, 5);
+    var month = time.slice(6, 8);
+    var day = time.slice(9, 11);
+    return month + '/' + day + '/' + year;
+  }
+  // 1. Load the JavaScript client library.
+  gapi.load('client', start);
